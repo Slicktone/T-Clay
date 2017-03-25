479ed1df83f6dab0002ab88d6eb7a181f64467a7
TonyUCLA <TonyUCLA>
2017-03-24 19:15:05 -0700

    Layout of components and importing

---
+++ src/components/Home.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+import Navbar from 'Navbar';
+import Header from './Header';
+import About from './About';
+import Projects from './Projects';
+import Contact from './Contact';
+import Footer from './Footer';
+
+class Home extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+
+    render() {
+        return(
+            <div className="home-content">
+                <Navbar />
+                <Header />
+                <Main />
+                {/*<About /> maybe?*/}
+                <Projects />
+                <Contact />
+                <Footer />
+</div>
+
+        );
+    }
+};
+
+export default Home;
