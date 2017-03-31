eb8435a49bd825001e3e49eedcb4b5f92ee895e8
TonyUCLA <TonyUCLA>
2017-03-30 22:03:14 -0700

    Nav bar component changed for proper styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,25 +4,22 @@ import ReactDOM from 'react-dom';
 class Navbar extends React.Component {
     render() {
         return(
+            <div className="container">
             <nav>
                 <a href="index.html" id="logo"> Tony Clayton </a>
-                    <div className="container">
-                        <ul className="navbar">
-                            <li>
+                
+                        <div className="navbar">
+                            
                                 <a href="index.html"> Home </a>
-                            </li>
-                            <li>
+                            
                                 <a href="#"> About </a>
-                            </li>
-                            <li>
+                            
                                  <a href="#"> Portfolio </a>
-                            </li>
-                            <li>
+                            
                                 <a href="#"> Contact </a>
-                            </li>
-                        </ul>              
-                </div>
-            </nav>
+                        </div>              
+                </nav>
+            </div>
         );
     }
 }
