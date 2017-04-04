e7e9d3a8db67256303be4a2fb980c4a1a33d3b16
TonyUCLA <TonyUCLA>
2017-04-03 21:19:08 -0700

    Skills positioning almost ready

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,31 +12,20 @@ class Home extends React.Component {
     render() {
         return(
             <div id="home-content">
-                <h1 className='name'> Tony Clayton </h1>
-                <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' alt='' />
+
+                <a className='skills-top' href='#' target='_blank'> HTML5 </a> <span className='space'> //////// </span> <a className='skills-top' href='#' target='_blank'> Javascript </a>
+                <div id='skills'>
+
+                    
+                 <a href='#' target='_blank'> CSS </a> <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' alt='' /> <a href='#' target='_blank'> jQuery </a>
+
+                     <br />
+                     <a href='#' target='_blank'> MySQL </a><span className='space'> //////// </span> <a href='#' target='_blank'> MongoDB </a>
+
                 <h3 className='profession'> Profession() => { " Web Developer " } </h3>
 
-                <div className='skills'>
-                    <a href='#' target='_blank'> HTML5 </a>
-                        <span className='space'> | </span>
-
-                    <a href='#' target='_blank'> CSS </a>
-                        <span className='space'> | </span>
-
-                    <a href='#' target='_blank'> Javascript </a>
-                        <span className='space'> | </span>
-
-                    <a href='#' target='_blank'> jQuery </a>
-                        <span className='space'> | </span>
-
-                    <a href='#' target='_blank'> MySQL </a>
-                        <span className='space'> | </span>
-
-                    <a href='#' target='_blank'> MongoDB </a>
-                        <span className='space'> | </span>
-
                </div>
-</div>
+        </div>
 
         );
     }
