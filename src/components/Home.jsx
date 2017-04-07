d41b39c46349485fc548417950c374c0a1037cd7
TonyUCLA <TonyUCLA>
2017-04-06 21:10:04 -0700

    Big push for Project Images and Styling (needs cleanup)

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -11,20 +11,43 @@ class Home extends React.Component {
 
     render() {
         return(
-            <div id="home-content">
+        <div id="home-content">
+            <div className='skills-first-row'>
+                <i className="devicon-html5-plain-wordmark colored"></i>
+                <i className="devicon-css3-plain-wordmark colored"></i>
+            </div>
+            
+            <br />
 
-                <i className="devicon-html5-plain-wordmark colored"></i> <span className='space'>      </span> <i className="devicon-javascript-plain colored"></i>
-                
-                <div id='skills'>
-              
-                 <i className="devicon-css3-plain-wordmark colored"></i> <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' alt='' /> <i className="devicon-jquery-plain-wordmark colored"></i>
+            <div className='skills-second-row'>
+                <span className='javascript'>
+                    <i className="devicon-javascript-plain colored"></i>
+                </span>
 
-                     <br />
-                     <i className="devicon-mysql-plain colored"></i> <span className='space'>      </span> <i className="devicon-mongodb-plain-wordmark colored"></i>
+                <span className='skill-spacing'>
+                </span>
 
-                <h3 className='profession'> Profession() => { " Web Developer " } </h3>
+                <span className='jquery'>
+                    <i className="devicon-jquery-plain-wordmark colored"></i>
+                </span>
+            </div>
 
-               </div>
+            <div className='img-container'>
+                <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' alt='' />
+            </div>
+
+            <div className='skills-third-row'>
+                <i className="devicon-nodejs-plain colored"></i>
+                <i className="devicon-react-original-wordmark colored"></i>
+            </div>
+
+            <div className='skills-fourth-row'>
+                <i className="devicon-mysql-plain colored"></i>
+                <i className="devicon-mongodb-plain-wordmark colored"></i>
+            </div>
+
+            <h3 className='profession'> Profession() => { " { Web Developer } " } </h3>
+
         </div>
 
         );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,30 +11,39 @@ class Projects extends React.Component {
                <div className='image-container'>
 
                    <div className='logo-container'>
-                       <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' href='www.t-clay.com'></img>
+                       <img className='tukio' src='../public/assets/img/tukio-180x180.png' href='https://tukio-webapp.herokuapp.com/'></img>
                             
                             <div className='project-info'>
-                                <a href='#' target='_blank'>
-                                    <h2> Project Title </h2>
+                                <a href='https://tukio-webapp.herokuapp.com/' target='_blank'>
+                                    <h2> Tukio </h2>
                                 </a>
                             </div>
                     </div>
 
                     <div className='logo-container'>
-                       <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' href='www.t-clay.com'></img>
+                       <img className='city-trek' src='../public/assets/img/city-trek-logo-150.png' href='https://city-trek.herokuapp.com/'></img>
+                         <div className='project-info'>
+                                <a href='https://city-trek.herokuapp.com/' target='_blank'>
+                                    <h2> City Trek </h2>
+                                </a>
+                            </div>
+                    </div>
+
+                    <div className='logo-container'>
+                       <img className='north-east-trees' src='../public/assets/img/NET-logo-250.png' href='www.bringingnatureback.org'></img>
                           
                           <div className='project-info'>
-                                <a href='#' target='_blank'>
-                                    <h2> Project Title </h2>
+                                <a href='www.bringingnatureback.org' target='_blank'>
+                                    <h2> North East Trees </h2>
                                 </a>
                             </div>
                     </div>
 
                     <div className='logo-container'>
-                       <img className='profile-shot' src='../public/assets/img/profile_shot.jpg' href='www.t-clay.com'></img>
+                       <img className='jslv' src='../public/assets/img/jslv-hooks-180.png' href='www.jslv.com'></img>
                          <div className='project-info'>
-                                <a href='#' target='_blank'>
-                                    <h2> Project Title </h2>
+                                <a href='www.jslv.com' target='_blank'>
+                                    <h2> JSLV </h2>
                                 </a>
                             </div>
                     </div>
